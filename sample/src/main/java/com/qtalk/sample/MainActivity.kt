2af39cf9b3fa59ce3f70b70048251ba35548e6e0
Suraj Shah <shah.suraj.k@gmail.com>
2020-11-26 13:53:54 +0530

    * [M] Changes sample app to use view pager 2

--- sample/src/main/java/com/qtalk/sample/MainActivity.kt
+++ sample/src/main/java/com/qtalk/sample/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.qtalk.sample
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.qtalk.sample.fragments.AdvancedFragment
 import com.qtalk.sample.fragments.BasicFragment
 import com.qtalk.sample.fragments.ContactsFragment
@@ -25,37 +25,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        view_pager.adapter = ViewPagerAdapter(supportFragmentManager, this)
-    }
 
-    private class ViewPagerAdapter constructor(
-        fm: FragmentManager,
-        val mContext: Context?
-    ) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-        //internal ViewPagerAdapter class, with 3 fragments.
-        override fun getItem(position: Int): Fragment {
-            return when (position) {
-                PAGE_INDEX_BASIC -> BasicFragment()
-                PAGE_INDEX_ADVANCE -> AdvancedFragment()
-                PAGE_INDEX_LANGUAGES -> ProgrammingLanguagesFragment()
-                PAGE_INDEX_CONTACTS -> ContactsFragment()
-                else -> throw IllegalArgumentException("Not expecting $position.")
+        view_pager.adapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount(): Int {
+                return VIEWPAGER_COUNT
+            }
+
+            override fun createFragment(position: Int): Fragment {
+                return when (position) {
+                    PAGE_INDEX_BASIC -> BasicFragment()
+                    PAGE_INDEX_ADVANCE -> AdvancedFragment()
+                    PAGE_INDEX_LANGUAGES -> ProgrammingLanguagesFragment()
+                    PAGE_INDEX_CONTACTS -> ContactsFragment()
+                    else -> throw IllegalArgumentException("Not expecting $position.")
+                }
             }
         }
 
-        override fun getCount(): Int {
-            return VIEWPAGER_COUNT
-        }
+        tabLayout.tabMode = TabLayout.MODE_SCROLLABLE
 
-        // for the pageTitleStrip View at the top of the viewpager
-        override fun getPageTitle(position: Int): CharSequence? {
-            when (position) {
-                PAGE_INDEX_BASIC -> return mContext?.resources?.getString(R.string.basic_fragment)
-                PAGE_INDEX_ADVANCE -> return mContext?.resources?.getString(R.string.advanced_fragment)
-                PAGE_INDEX_LANGUAGES -> return mContext?.resources?.getString(R.string.programming_languages_fragment)
-                PAGE_INDEX_CONTACTS -> return mContext?.resources?.getString(R.string.contacts_fragment)
+        TabLayoutMediator(
+            tabLayout, view_pager, true, true
+        ) { tab, position ->
+            tab.text = when (position) {
+                PAGE_INDEX_BASIC -> resources?.getString(R.string.basic_fragment)
+                PAGE_INDEX_ADVANCE -> resources?.getString(R.string.advanced_fragment)
+                PAGE_INDEX_LANGUAGES -> resources?.getString(R.string.programming_languages_fragment)
+                PAGE_INDEX_CONTACTS -> resources?.getString(R.string.contacts_fragment)
+                else -> throw IndexOutOfBoundsException("View pager size is $VIEWPAGER_COUNT, can't access $position")
             }
-            return ""
-        }
+        }.attach()
     }
 }
--- sample/src/main/java/com/qtalk/sample/adapters/BasicAdapter.kt
+++ sample/src/main/java/com/qtalk/sample/adapters/BasicAdapter.kt
@@ -10,7 +10,7 @@ import com.qtalk.recyclerviewfastscroller.RecyclerViewFastScroller
 import com.qtalk.sample.R
 import kotlinx.android.synthetic.main.recycler_view_list_item.view.*
 
-class BasicAdapter(private val mContext: Context?) :
+class BasicAdapter(private val context: Context?) :
     RecyclerView.Adapter<BasicAdapter.ViewHolder>(), RecyclerViewFastScroller.OnPopupTextUpdate {
 
     override fun onChange(position: Int): CharSequence {
@@ -19,18 +19,18 @@ class BasicAdapter(private val mContext: Context?) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
-            LayoutInflater.from(mContext)
+            LayoutInflater.from(context)
                 .inflate(R.layout.recycler_view_list_item_center, parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.mTextView.text = (position + 1).toString()
+        holder.textView.text = (position + 1).toString()
     }
 
     override fun getItemCount() = 100
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val mTextView: TextView = view.text_view
+        val textView: TextView = view.text_view
     }
 }
